35ad48239471c38c7883141a017d076f1c93a3d5
Solomon Muoghara <nerdsolomon@gmail.com>
2025-12-22 11:48:43 +0100

    added searchbar

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -1,7 +1,8 @@
 import { FaUserCircle } from "react-icons/fa";
 import { MdMenu } from "react-icons/md";
 import { useNavigate } from "react-router";
-import {useData} from "~/hooks/useData";
+import { useData } from "~/hooks/useData";
+import { Searchbar } from "./searchbar";
 
 interface Prop {
   isOpen: boolean;
@@ -9,21 +10,36 @@ interface Prop {
 }
 
 const Navbar = ({ isOpen, onClose }: Prop) => {
-  const {currentUser} = useData()
-  const navigate = useNavigate()
+  const { currentUser } = useData();
+  const navigate = useNavigate();
   return (
-    <nav className="border-b border-gray-300 p-4 sticky top-0 z-50 flex justify-between bg-white text-blue-500 font-bold">
+    <nav className="border-b border-gray-300 p-4 sticky top-0 z-50 grid grid-cols-3 bg-white text-blue-500 font-bold">
       <div className="flex items-center gap-2">
-        <button onClick={() => onClose(!isOpen)} className="lg:hidden text-xl cursor-pointer p-2 transition rounded-lg hover:bg-gray-100">
+        <button
+          onClick={() => onClose(!isOpen)}
+          className="lg:hidden text-xl cursor-pointer p-2 transition rounded-lg hover:bg-gray-100"
+        >
           <MdMenu />
         </button>
         <span className="text-xl ">RentRight</span>
-        
       </div>
-      
-      <div className="items-center flex gap-2 hover:bg-gray-100 rounded-full px-2 py-2" onClick={() => navigate("/profile")}>
-        <FaUserCircle size={30} />
-        <span className="hidden lg:block">{currentUser.company ? currentUser.company : `${currentUser.firstName} ${currentUser.lastName}`}</span>
+
+      <div className="flex items-center lg:justify-center">
+        <Searchbar />
+      </div>
+
+      <div className="flex justify-end">
+        <div
+          className="items-center flex gap-2 hover:bg-gray-100 rounded-full px-2 py-2"
+          onClick={() => navigate("/profile")}
+        >
+          <FaUserCircle size={30} />
+          <span className="hidden lg:block">
+            {currentUser.company
+              ? currentUser.company
+              : `${currentUser.firstName} ${currentUser.lastName}`}
+          </span>
+        </div>
       </div>
     </nav>
   );
---
+++ app/components/layout/searchbar.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import { RiSearch2Line } from "react-icons/ri";
+
+export const Searchbar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (isExpanded) {
+      inputRef.current?.focus();
+    }
+  }, [isExpanded]);
+
+  return (
+    <div className="flex items-center">
+      <button
+        onClick={() => setIsExpanded(true)}
+        className={`lg:hidden rounded-full bg-white p-2 hover:bg-gray-100 ${
+          isExpanded ? "invisible" : "visible"
+        }`}
+      >
+        <RiSearch2Line size={20} />
+      </button>
+
+      <div
+        className={`
+          absolute inset-0 z-50 flex items-center pr-2
+          lg:static lg:z-auto lg:flex
+          ${isExpanded ? "flex" : "hidden"} lg:flex
+        `}
+      >
+        <div className="absolute left-1/2 -translate-x-1/2 w-[90vw] lg:static lg:translate-x-0 lg:w-[600px]">
+          <RiSearch2Line className="absolute left-3 top-1/2 -translate-y-1/2" />
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Search"
+            onBlur={() => setIsExpanded(false)}
+            className="w-full pl-10 pr-3 py-2 text-sm font-normal rounded-full bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-300"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
