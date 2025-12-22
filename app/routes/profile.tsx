3451e26f1e238c9d4d7e57f12ec04182396eb9ba
Solomon Muoghara <nerdsolomon@gmail.com>
2025-12-22 09:27:14 +0100

    g

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -9,7 +9,7 @@ const Profile = () => {
     `RentRight - Profile : ${currentUser.firstName} ${currentUser.lastName}`
   );
   return (
-    <div className="p-4">
+    <div className="py-4 px-6">
       <div className="flex justify-center">
         <div className="w-[30%] border-4 mb-2 rounded-full aspect-square bg-gray-300 flex capitalize items-center justify-center text-white text-[40px] lg:text-[50px] font-bold">
           {currentUser?.imageUrl ? (
@@ -25,32 +25,32 @@ const Profile = () => {
 
       <div className="space-y-2">
         {currentUser.company && (
-          <div className="flex justify-between items-center text-sm text-gray-500">
-            <div className="text-gray-400">Company</div>
+          <div className="flex justify-between items-center text-gray-500">
+            <div className="text-gray-400 font-bold">Company</div>
             <div className="text-gray-400">{currentUser.company}</div>
           </div>
         )}
-        <div className="flex justify-between items-center text-sm text-gray-500">
-          <div className="text-gray-400">Name</div>
+        <div className="flex justify-between items-center text-gray-500">
+          <div className="text-gray-400 font-bold">Name</div>
           <div className="text-gray-400">
             {currentUser.firstName} {currentUser.lastName}
           </div>
         </div>
-        <div className="flex justify-between items-center text-sm text-gray-500">
-          <div className="text-gray-400">Email</div>
+        <div className="flex justify-between items-center text-gray-500">
+          <div className="text-gray-400 font-bold">Email</div>
           <div className="text-gray-400">
             {currentUser.email}
           </div>
         </div>
-        <div className="flex justify-between items-center text-sm text-gray-500">
-          <div className="text-gray-400">Phone</div>
+        <div className="flex justify-between items-center text-gray-500">
+          <div className="text-gray-400 font-bold">Phone</div>
           <div className="text-gray-400">
             {currentUser.phone}
           </div>
         </div>
       </div>
 
-      <div className="flex justify-end gap-2 mt-2">
+      <div className="flex justify-end gap-2 mt-4">
         <Editprofile />
         <Logout/>
       </div>
