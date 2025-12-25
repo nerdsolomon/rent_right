f6de0b9c7ead4c9aa7f905caafa632128d5070a7
Solomon Muoghara <nerdsolomon@gmail.com>
2025-12-25 17:04:01 +0100

    h

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -14,12 +14,15 @@ const Navbar = ({ isOpen, onClose }: Prop) => {
   const navigate = useNavigate();
   return (
     <nav className="border-b border-gray-300 p-2 sticky top-0 z-50 grid grid-cols-3 bg-white text-blue-500 font-bold">
-      <div className="flex items-center gap-4 text-xl">
-        <MdMenu
+      <div className="flex items-center gap-4">
+        <button
+          className="lg:hidden ml-2 cursor-pointer hover:text-blue-700 text-lg"
           onClick={() => onClose(!isOpen)}
-          className="lg:hidden ml-2 cursor-pointer hover:text-blue-700"
-        />
-        <span className="lg:px-2">RentRight</span>
+        >
+          <MdMenu />
+        </button>
+
+        <span className="lg:px-2 text-xl">RentRight</span>
       </div>
 
       <div className="flex items-center justify-center">
