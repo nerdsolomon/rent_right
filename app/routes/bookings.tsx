08ff830220567df92f1031259be050542f1bc92d
Solomon Muoghara <nerdsolomon@gmail.com>
2026-03-05 16:33:18 +0100

    change booking icon

--- app/routes/bookings.tsx
+++ app/routes/bookings.tsx
@@ -1,22 +1,40 @@
-import { FaComments } from "react-icons/fa";
+import { FaCalendarAlt } from "react-icons/fa";
+import { useData } from "~/hooks/useData";
 import { usePageTitle } from "~/hooks/usePageTitle";
 import { RequireAuth } from "~/hooks/useRequireAuth";
 
 const Bookings = () => {
   usePageTitle("RentRight - Bookings");
+  const { bookings } = useData();
 
   return (
     <RequireAuth>
-      <div className="flex justify-center">
-        <div className="mt-40 text-green-300">
-          <div className="flex justify-center">
-            <FaComments size={40} className="mb-5" />
-          </div>
-          <p className="text-center font-bold text-[22px]">
-            Coming soon!
-          </p>
-        </div>
-      </div>
+      <div className="space-y-2">
+              {bookings.length > 0 ? (
+                bookings.map((b, index) => (
+                  <div
+                    key={index}
+                    className="p-2 flex items-center w-full text-sm mb-2 border-b border-gray-300"
+                  >
+                    <div className="items-center">
+                      <span className="font-bold mr-2 capitalize">{b.property.title}</span>
+                      <p>{b.day}</p>
+                      <p>{b.time}</p>
+                      <p className="capitalize">{b.user.firstName}</p>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div className="mt-40">
+                  <div className="flex justify-center">
+                    <FaCalendarAlt size={40} className="text-gray-300 mb-5" />
+                  </div>
+                  <p className="text-center font-bold text-[22px] text-gray-300">
+                    No bookings
+                  </p>
+                </div>
+              )}
+            </div>
     </RequireAuth>
   );
 };
