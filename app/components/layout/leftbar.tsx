0ad7efa44a3832704ff8ba8ae9815ffd5daf70be
Solomon Muoghara <nerdsolomon@gmail.com>
2026-03-05 15:05:07 +0100

    change booking icon

--- app/components/layout/leftbar.tsx
+++ app/components/layout/leftbar.tsx
@@ -1,8 +1,8 @@
 import {
   FaBell,
   FaBriefcase,
+  FaCalendarAlt,
   FaChartBar,
-  FaComments,
   FaExclamationCircle,
   FaHome,
 } from "react-icons/fa";
@@ -22,7 +22,7 @@ const Leftbar = ({ isOpen, onClose }: Prop) => {
     { title: "Home", path: "/home", icon: FaHome, always: true },
     { title: "Dashboard", path: "/dashboard", icon: FaChartBar, restricted: true },
     { title: "Feedbacks", path: "/feedbacks", icon: FaExclamationCircle, restricted: true },
-    { title: "Bookings", path: "/bookings", icon: FaComments, restricted: false },
+    { title: "Bookings", path: "/bookings", icon: FaCalendarAlt, restricted: false },
     { title: "Notifications", path: "/notifications", icon: FaBell, restricted: false },
     { title: "Portfolio", path: `/portfolio/${currentUser.id}`, icon: FaBriefcase, restricted: false, owner: true },
   ];
