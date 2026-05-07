7030e967df5271d8efdc0a888eca3b11c53de559
Solomon Muoghara <nerdsolomon@gmail.com>
2026-05-07 12:20:18 +0100

    fix auth

--- app/routes/callback.tsx
+++ app/routes/callback.tsx
@@ -1,28 +1,34 @@
 import { useEffect } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
-import { authService } from "~/services/auth.service";
 import { authKeys } from "~/hooks/useAuth";
+import { setToken } from "~/services";
 
 const GoogleCallback = () => {
   const navigate = useNavigate();
   const qc = useQueryClient();
 
   useEffect(() => {
-    const finishLogin = async () => {
-      const user = await qc.fetchQuery({
+  const finishLogin = async () => {
+    const params = new URLSearchParams(window.location.search);
+
+    const token = params.get("token");
+
+    if (token) {
+      setToken(token);
+
+      await qc.invalidateQueries({
         queryKey: authKeys.me,
-        queryFn: authService.me,
       });
 
-      if (user) {
-        navigate("/home", { replace: true });
-      }
-    };
+      navigate("/home", { replace: true });
+    } else {
+      navigate("/", { replace: true });
+    }
+  };
 
-    finishLogin();
-  }, [navigate, qc]);
-
+  finishLogin();
+}, []);
   return <p>Signing you in...</p>;
 };
 
